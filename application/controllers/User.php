be8e4841d8f5ff6dccf7468f2a1c38e9eb293da8
artercode <arter@gmail.com>
2020-03-05 19:35:53 +0700

    04. Login

---
+++ application/controllers/User.php
@@ -0,0 +1,13 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class User extends CI_Controller
+{
+   public function index()
+   {
+      // mengambil semua data dr tabel database berdasarkan email yang ada di session
+      $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+      echo 'Selamat datang ' . $data['user']['name'];
+   }
+}
